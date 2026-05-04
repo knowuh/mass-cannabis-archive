ac5c98a917fbde28146dfaefa8caeeea88c74f42
paesseln <paesseln@gcc.mass.edu>
2026-05-04 10:47:20 -0400

    chore: align data structure with spec and add preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://knowuh.github.io',
-  base: '/mass-cannabis-archive', // Adjust if this is the repo name
+
+  // Adjust if this is the repo name
+  base: '/mass-cannabis-archive',
+
+  integrations: [preact()]
 });
